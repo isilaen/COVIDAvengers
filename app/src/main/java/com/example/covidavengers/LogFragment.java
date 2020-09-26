57686de79a6d750500b2a61653997a6e2b1bbebb
isilaen <43592303+isilaen@users.noreply.github.com>
2020-09-26 21:22:30 +1000

    fixed bug on log page

--- app/src/main/java/com/example/covidavengers/LogFragment.java
+++ app/src/main/java/com/example/covidavengers/LogFragment.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LogFragment extends Fragment {
 
+    private Button mondayBtn;
+    private Button tuesdayBtn;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_log, container, false);
+//
+//        mondayBtn = layout.findViewById(R.id.mondayBtn);
+//        tuesdayBtn = layout.findViewById(R.id.tuesdayBtn);
+//
+//        return layout;
     }
 }
