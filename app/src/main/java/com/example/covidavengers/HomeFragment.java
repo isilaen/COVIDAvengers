9544cfce43a1ce72750e880539a243f6ece12b13
Bridget Gatt <bgatt318@gmail.com>
2020-09-27 01:49:17 +1000

    changing UI stuff

--- app/src/main/java/com/example/covidavengers/HomeFragment.java
+++ app/src/main/java/com/example/covidavengers/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View layout = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView tv1 = (TextView)layout.findViewById(R.id.welcomeTextView);
+        tv1.setText("Welcome, " + User.getInstance().getFirstName()+ ".");
+        return layout;
     }
 }
